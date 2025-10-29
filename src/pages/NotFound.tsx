52aaf40d612ccf3854e0be023a411008f5bbc040
soranonvalid <nidsora25@gmail.com>
2025-10-29 21:57:01 +0700

    notFound fill

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router";
+
 const NotFound = () => {
-  return <div>Not Found</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col gap-5 items-center justify-center ">
+        <img width={70} src="/locales/lostFirePlace.gif" alt="" />
+        <h1 className="text-7xl">404</h1>
+        <p className="text-sm">
+          It seems you travel too far...{"  "}
+          <a
+            onClick={() => {
+              navigate(-1);
+            }}
+            className="italic underline"
+          >
+            it doesnt mean you lost.
+          </a>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
